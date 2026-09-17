0ae0395e0d69e17dd1673f212982da9f55494bc9
Dominic juma <jumadomi518@gmail.com>
2026-09-17 13:50:11 +0300

    added ws

--- supabase.js
+++ supabase.js
@@ -1,12 +1,15 @@
 
 
 import { createClient } from "@supabase/supabase-js";
+import WebSocket from "ws";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY,
   {
-    realtime: { enabled: false }
+    realtime: {
+      transport: WebSocket
+    }
   }
 );
 
