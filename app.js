02dd02f006337b20510359e61b3c602e129b857e
Dominic juma <jumadomi518@gmail.com>
2026-09-17 14:17:10 +0300

    fixing error

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 import { supabase } from "./supabase.js";
 import webpush from "web-push";
 import dotenv from "dotenv";
