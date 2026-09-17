d64f70972136f4f67856dba2c79feb51471403ff
Dominic juma <jumadomi518@gmail.com>
2026-09-17 13:42:11 +0300

    added ws

--- supabase.js
+++ supabase.js
@@ -1,7 +1,13 @@
 
- import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  {
+    realtime: { enabled: false }
+  }
 );
+
+export { supabase };
